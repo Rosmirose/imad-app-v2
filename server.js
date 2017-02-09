fb54dba8b3f365ba8596a041864fc3fa3a4565a5
Rosmi <rosmirehman000@gmail.com>
2017-02-09 22:59:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ var htmlTemplate = `
         </body>
     </html>
     `;
- return htmltemplate;
+ return htmlTemplate;
 }
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
