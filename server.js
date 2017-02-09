ef6bf0790ac0221f5c67075c5eb659e258890f75
Rosmi <rosmirehman000@gmail.com>
2017-02-09 23:14:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.get('/', function (req, res) {
 
 app.get('/:articleName', function(req, res) {
     var articleName = req.params.articleName;
-    res.send(createTemplate(articles[artcileName]));
+    res.send(createTemplate(articles[articleName]));
 })
 
 app.get('/ui/style.css', function (req, res) {
