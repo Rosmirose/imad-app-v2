080cc4ce1173346d1691d9d2b0111f7d791f6c1c
Rosmi <rosmi@systeminsights.com>
2017-02-10 22:20:11 +0530

    edited main to send request to /counter and get response

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,22 @@
-console.log('Loaded!');
+var button = document.getElementById('counter');
+// var counter = 0;
+button.onclick = function () {
+  // create a request object to the counter end point
+  var request = new XMLHttpRequest();
 
-// change the text of the maintxt div
-
-var element = document.getElementById('maintext');
-element.innerHTML = "New value";
-
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight() {
-  marginLeft = marginLeft + 1;
-  img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function moveImage() {
-  var interval = setInterval(moveRight, 50);
-  // img.style.marginLeft = '100px';
+  //capture response and store in a var
+  request.onreadystatechange = function() {
+    if(request.readyState === XMLHttpRequest.DONE) {
+      if(request.status === 200) {
+        console.log('STATUS 200')
+        // render the variable in correct span
+        var counter = request.responseText;
+        var span = document.getElementById('count');
+        span.innerHTML = counter.toString();
+      }
+    }
+  }
+  //make the request
+  request.open('GET','http://rosmirose.imad.hasura-app.io/counter', true);
+  request.send(null);
 }
