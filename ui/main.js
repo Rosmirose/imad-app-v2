c76e42c5d823237dbff5cd91faa4fb27c2a39f72
Rosmi <rosmirehman000@gmail.com>
2017-02-10 23:35:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,7 +28,7 @@ var submit = document.getElementById('submit_bn');
 submit.onclick = function() {
     var names= ['name1', 'name2', 'name3', 'name4'];
     var list = '';
-    for (var i=0; i<names.length; i++) {
+    for (var i=1; i<names.length; i++) {
         list += '<li>' + name[i] + '</li>';
     }
     var ul = document.getElementById('namelist');
