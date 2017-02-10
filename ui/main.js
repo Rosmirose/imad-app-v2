9c7b3e221e06073941142e52ab21491c9674f9c1
Rosmi <rosmirehman000@gmail.com>
2017-02-11 00:03:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,15 +22,27 @@ button.onclick = function () {
   request.send(null);
 };
 
+//submit name
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_bn');
 submit.onclick = function() {
-    var names= ['name1', 'name2', 'name3', 'name4'];
-    var list = '';
-    for (var i=0; i<names.length; i++) {
-        list += '<li>' + names[i] + '</li>';
+    
+     request.onreadystatechange = function() {
+    if(request.readyState === XMLHttpRequest.DONE) {
+      if(request.status === 200) {
+        var names= request.responseText;
+        names = JSON.parse(names);
+        var list = '';
+        for (var i=0; i<names.length; i++) {
+            list += '<li>' + names[i] + '</li>';
+        }
+        var ul = document.getElementById('namelist');
+        ul.innerHTML = list;
+      }
     }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+  };
+   
+  request.open('GET','http://rosmirose.imad.hasura-app.io/submit-name?name='+name, true);
+  request.send(null); 
 };
