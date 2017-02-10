dcb72ae00f702ac39fbb6304dd3b8b1c7b7fc96f
Rosmi <rosmirehman000@gmail.com>
2017-02-11 00:06:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -43,6 +43,6 @@ submit.onclick = function() {
     }
   };
    
-  request.open('GET','http://rosmirose.imad.hasura-app.io/submit-name?name='+name, true);
+  request.open('GET','http://rosmirose.imad.hasura-app.io/submit-name?name=' + name, true);
   request.send(null); 
 };
