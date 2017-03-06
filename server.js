55db85080d4c725c46e85918eba2f949d90dad64
Rosmi <rosmirehman000@gmail.com>
2017-03-06 21:58:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
 
 var app = express();
+var config = {
+    user: 'rosmirose',
+    database: 'rosmirose',
+    host: 'http://db.imad.hasura-app.io/',
+    port: '5432',
+    password: DB_PASSWORD
+}
+
+
 app.use(morgan('combined'));
 
 var articles = {
@@ -99,6 +109,23 @@ app.get('/counter', function(req,res) {
 });
 
 var names = [];
+
+var pool = new Pool(config)
+
+app.get('/test-db', function (req, res) {
+  // make a select request
+  pool.query('SELECT * FROM test', function(err, result) {
+      if(err) {
+        res.status(500).send(err.toString())
+      } else {
+        res.send(JSON.stringify(result))  
+      }
+  }) 
+      
+  
+  // return response with the results
+});
+
 app.get('/submit-name', function(req, res) { 
   //get name from req object
   var name = req.query.name;
