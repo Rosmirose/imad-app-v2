0bfa6b20d4baf44cf6a23f304d912b50573db25f
Rosmi <rosmirehman000@gmail.com>
2017-03-06 22:16:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,13 +102,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var counter = 0;
-app.get('/counter', function(req,res) {
-    counter = counter+1;
-    res.send(counter.toString());
-});
+// var counter = 0;
+// app.get('/counter', function(req,res) {
+//     counter = counter+1;
+//     res.send(counter.toString());
+// });
 
-var names = [];
+// var names = [];
 
 var pool = new Pool(config)
 
@@ -126,31 +126,31 @@ app.get('/test-db', function (req, res) {
   // return response with the results
 });
 
-app.get('/submit-name', function(req, res) { 
-  //get name from req object
-  var name = req.query.name;
-  names.push(name);
-  res.send(JSON.stringify(names));
-});
+// app.get('/submit-name', function(req, res) { 
+//   //get name from req object
+//   var name = req.query.name;
+//   names.push(name);
+//   res.send(JSON.stringify(names));
+// });
 
 app.get('/:articleName', function(req, res) {
     var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
+// app.get('/ui/style.css', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+// });
 
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
+// app.get('/ui/main.js', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+// });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
+// app.get('/ui/madi.png', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+// });
 
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
+// var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+// app.listen(8080, function () {
+//   console.log(`IMAD course app listening on port ${port}!`);
+// });
