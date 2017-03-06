28edd35c5dff1241d8a13981719276a1e09f4ad3
Rosmi <rosmirehman000@gmail.com>
2017-03-06 22:57:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -133,9 +133,22 @@ app.get('/submit-name', function(req, res) {
   res.send(JSON.stringify(names));
 });
 
-app.get('/:articleName', function(req, res) {
-    var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+app.get('/article/:articleName', function(req, res) {
+    // var articleName = req.params.articleName;
+    // res.send(createTemplate(articles[articleName]));
+    
+    pool.query("SELECT * FROM article where title =" + req.params.articleName, function(err, result) {
+      if(err) {
+        res.status(500).send(err.toString())
+      } else {
+        if (result.rows.length === 0) {
+          res.status(404).send("Article Not Found"); 
+        } else {
+            var articleData = result.rows[0];
+            res.send(createTemplate(articleData));
+        }
+      }
+    })
 });
 
 app.get('/ui/style.css', function (req, res) {
