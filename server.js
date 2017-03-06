c5d7c6d671e4b07b15223d1e96d578b94d167d2d
Rosmi <rosmirehman000@gmail.com>
2017-03-06 22:57:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -133,7 +133,7 @@ app.get('/submit-name', function(req, res) {
   res.send(JSON.stringify(names));
 });
 
-app.get('/article/:articleName', function(req, res) {
+app.get('/articles/:articleName', function(req, res) {
     // var articleName = req.params.articleName;
     // res.send(createTemplate(articles[articleName]));
     
