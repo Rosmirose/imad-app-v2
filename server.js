51140667ee2e078bd9ba822018d0d79989e1891e
Rosmi <rosmirehman000@gmail.com>
2017-03-14 20:43:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 
 var app = express();
 var config = {
@@ -97,6 +98,15 @@ var htmlTemplate = `
     `;
  return htmlTemplate;
 }
+
+function hash (input, salt) {
+  var hashed = crypto.pbkdf2Sync(input, salt, 100000, 512, 'sha512');
+  return hashed.toString('hex');
+}
+app.get('/hash/:input', function(req, res) {
+  var hashedString = hash(req.params.input, salt);
+  res.send(hashedString);
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
