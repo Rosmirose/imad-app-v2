27b3afa488c345979b24cecf71ebe632642fffe6
Rosmi <rosmirehman000@gmail.com>
2017-03-22 00:10:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -156,7 +156,7 @@ app.post('/login', function(req, res) {
 
 app.get('/check-login', function(req, res) {
    if (req.session && req.session.auth && req.session.auth.userId) {
-       res.send(`You are logged in:`, + req.session.auth.userId.toString());
+       res.status(200).send(`You are logged in:`, + req.session.auth.userId.toString());
    } else {
        res.send("Not logged in");
    }
