2b955a03cc3aaf85861938bce75f4b1f5121488c
Rosmi <rosmirehman000@gmail.com>
2017-03-21 22:30:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -56,7 +56,7 @@ submit.onclick = function() {
   var password =  document.getElementById('password').value;
   console.log(username, password);
   var name = nameInput.value;
-  request.open('POST','http://rosmirose.imad.hasura-app.io/submit-name?name=' + name, true);
+  request.open('POST','http://rosmirose.imad.hasura-app.io/login', true);
   request.setRequestHeader('Content-Type', 'application/json');
   request.send(JSON.stringify({username: username, password: password})); 
 };
