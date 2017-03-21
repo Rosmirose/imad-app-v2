c6cf03b67e5155f297a44671d923e1ea921b82c7
Rosmi <rosmirehman000@gmail.com>
2017-03-21 22:20:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,22 +5,25 @@ button.onclick = function () {
   var request = new XMLHttpRequest();
 
   // capture response and store in a var
-  request.onreadystatechange = function() {
-    if(request.readyState === XMLHttpRequest.DONE) {
-      if(request.status === 200) {
-        // render the variable in correct span
-        var counter = request.responseText;
-        var span = document.getElementById('count');
-        span.innerHTML = counter.toString();
-      }
-    }
-  };
-   
-  //make the request
   
-  request.open('GET','http://rosmirose.imad.hasura-app.io/counter', true);
-  request.send(null);
-};
+//   request.onreadystatechange = function() {
+//     if(request.readyState === XMLHttpRequest.DONE) {
+//       if(request.status === 200) {
+//         // render the variable in correct span
+//         var counter = request.responseText;
+//         var span = document.getElementById('count');
+//         span.innerHTML = counter.toString();
+//       }
+//     }
+//   };
+   
+//   //make the request
+  
+//   request.open('GET','http://rosmirose.imad.hasura-app.io/counter', true);
+//   request.send(null);
+// };
+
+
 
 //submit name
 
@@ -31,20 +34,29 @@ submit.onclick = function() {
     request.onreadystatechange = function() {
     if(request.readyState === XMLHttpRequest.DONE) {
       if(request.status === 200) {
-        var names= request.responseText;
-        names = JSON.parse(names);
-        var list = '';
-        for (var i=0; i<names.length; i++) {
-            list += '<li>' + names[i] + '</li>';
-        }
-        var ul = document.getElementById('namelist');
-        ul.innerHTML = list;
+          console.log("User logged in");
+          alert("Logged in successfully");
+        // var names= request.responseText;
+        // names = JSON.parse(names);
+        // var list = '';
+        // for (var i=0; i<names.length; i++) {
+        //     list += '<li>' + names[i] + '</li>';
+        // }
+        // var ul = document.getElementById('namelist');
+        // ul.innerHTML = list;
+      } else if(request.status === 403){
+          alert("Username/password invalid");
+      } else if(request.status === 500) {
+          alert("Something went wrong");
       }
     }
   };
   
-  var nameInput = document.getElementById('name');
+  var nameInput = document.getElementById('username').value;
+  var password =  document.getElementById('password').value;
+  console.log(username, password)
   var name = nameInput.value;
-  request.open('GET','http://rosmirose.imad.hasura-app.io/submit-name?name=' + name, true);
-  request.send(null); 
+  request.open('POST','http://rosmirose.imad.hasura-app.io/submit-name?name=' + name, true);
+  request.setRequestHeader('Content-Type', 'application/json')
+  request.send(JSON.stringify({username: username, password: password})); 
 };
